02d33d9b79218b9ae2270a33350654ad711b7ae7
Sonali Jamadar <99808291+sonalijamadar@users.noreply.github.com>
2026-01-08 15:59:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
 //your JS code here. If required.
+const output = document.getElementById("output");
+output.innerHTML = `
+  <tr id="loading">
+    <td colspan="2">Loading...</td>
+  </tr>
+`;
+
+function createPromise() {
+  const time = Math.random() * 2 + 1; // random between 1 and 3 seconds
+
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve(time);
+    }, time * 1000);
+  });
+}
+const promise1 = createPromise();
+const promise2 = createPromise();
+const promise3 = createPromise();
+
+Promise.all([promise1, promise2, promise3]).then((results) => {
+  output.innerHTML = "";
+  let maxTime = Math.max(...results);
+	results.forEach((time, index) => {
+    const row = document.createElement("tr");
+    row.innerHTML = `
+      <td>Promise ${index + 1}</td>
+      <td>${time.toFixed(3)}</td>
+    `;
+    output.appendChild(row);
+  });
+
+	const totalRow = document.createElement("tr");
+  totalRow.innerHTML = `
+    <td>Total</td>
+    <td>${maxTime.toFixed(3)}</td>
+  `;
+  output.appendChild(totalRow);
+});
+	
